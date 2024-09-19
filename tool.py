f915361c7c03f880910e7773cc683aadb44927bb
lalakiop <1442059261@qq.com>
2024-09-19 15:07:17 +0800

    Update tool.py
    增加日志输出

--- tool.py
+++ tool.py
@@ -13,14 +13,14 @@ def find_and_remove_obfuscation(input_file_path, header_signatures):
     :param input_file_path: 输入文件路径
     :param header_signatures: 特定标识符列表
     """
-    print(input_file_path)
+    log_message(input_file_path)
     with open(input_file_path, 'rb') as input_file:
         input_data = input_file.read()
         
         # 检查文件是否以特定标识符开头
         for header_signature in header_signatures:
             if input_data.startswith(header_signature):
-                print("无需去混淆")
+                log_message("无需去混淆")
                 return
 
         # 检查前150个字符是否包含特定标识符
@@ -28,7 +28,7 @@ def find_and_remove_obfuscation(input_file_path, header_signatures):
             if header_signature in input_data[:150]:
                 # 提取文件中 "CAB-" 后面的 33 位字符
                 cab_matches = re.findall(b'CAB-(.{33})', input_data)
-                print(cab_matches)
+                log_message(cab_matches)
                 
                 if cab_matches:
                     cab_char = cab_matches[0].decode('utf-8',errors='ignore')
@@ -43,13 +43,13 @@ def find_and_remove_obfuscation(input_file_path, header_signatures):
                                 existing_chars.add(existing_cab_char)
 
                     if cab_char in existing_chars:
-                        print("数据已存在")
+                        log_message("数据已存在")
                         # 备份文件到当前目录下，然后删除文件中特定标识符前面的字符，不包括标识符本身
                         #backup_file_path = input_file_path + ".backup"
                         #shutil.copyfile(input_file_path, backup_file_path)
                         with open(input_file_path, 'wb') as original_file:
                             original_file.write(input_data[input_data.find(header_signature):])
-                        print("已完成去混淆")
+                        log_message("已完成去混淆")
                     else:
                         # 提取文件名
                         file_name = os.path.basename(input_file_path)
@@ -58,17 +58,17 @@ def find_and_remove_obfuscation(input_file_path, header_signatures):
                         # 存储信息到 mate.ini
                         with open("mate.ini", 'a', encoding='utf-8') as mate_file:
                             mate_file.write(f"{cab_char}-{file_name}-{deleted_chars}-{input_file_path}\n")
-                            print("数据已记录："f"{cab_char}-{file_name}-{deleted_chars}-{input_file_path}")
+                            log_message("数据已记录："f"{cab_char}-{file_name}-{deleted_chars}-{input_file_path}")
                         # 备份文件到当前目录下，然后删除文件中特定标识符前面的字符，不包括标识符本身
                         #backup_file_path = input_file_path + ".backup"
                         #shutil.copyfile(input_file_path, backup_file_path)
                         with open(input_file_path, 'wb') as original_file:
                             original_file.write(input_data[input_data.find(header_signature):])
-                        print("已完成去混淆")
+                        log_message("已完成去混淆")
                     return
 
         # 如果没有匹配的标识符，输出 "不是标准的 Unity 文件"
-        print("不是标准的 Unity 文件")
+        log_message("不是标准的 Unity 文件")
 
 def restore_obfuscation(input_file_path, header_signatures):
     """
@@ -116,7 +116,7 @@ def restore_obfuscation(input_file_path, header_signatures):
                                     content = renamed_file.read()
                                     renamed_file.seek(0)
                                     renamed_file.write(bytes.fromhex(deleted_chars_data) + content)
-                                print(f"已完成还原混淆")
+                                log_message(f"已完成还原混淆")
                             else:
                                 # 复制选中的文件并命名为mate.ini中"CAB-"后面字符的文件名称
                                 new_file_path = os.path.join(os.path.dirname(input_file_path), rename_filename)
@@ -126,21 +126,21 @@ def restore_obfuscation(input_file_path, header_signatures):
                                     content = renamed_file.read()
                                     renamed_file.seek(0)
                                     renamed_file.write(bytes.fromhex(deleted_chars_data) + content)
-                                    print(f"已完成还原混淆和文件名称")
+                                    log_message(f"已完成还原混淆和文件名称")
                                 
                                 # 删除原文件
                                 input_file.close()
                                 os.remove(input_file_path)
                         else:
-                            print("文件名称未找到")
+                            log_message("文件名称未找到")
                     else:
-                        print("数据库无记录")
+                        log_message("数据库无记录")
                 else:
-                    print("文件中未找到 CAB- 后面的 33 位字符")
+                    log_message("文件中未找到 CAB- 后面的 33 位字符")
                 return
 
         # 如果文件不以特定标识符开头，输出 "无需还原混淆"
-        print("无需还原混淆")
+        log_message("无需还原混淆")
 
 def browse_and_remove_obfuscation():
     """
@@ -247,6 +247,7 @@ def show_context_menu(event):
         context_menu.grab_release()
 
 def open_mate_ini():
+
     """
     打开 "mate.ini" 文件并更新列表框内容
     """
@@ -257,6 +258,19 @@ def open_mate_ini():
             if match:
                 content_between_hyphens = match.group(1)
                 file_listbox.insert(0, content_between_hyphens)
+
+
+def log_message(message):
+    if isinstance(message, list):
+        message = [m.decode('utf-8') if isinstance(m, bytes) else str(m) for m in message]
+        log_text.insert(tk.END, ''.join(message) + '\n')
+    else:
+        log_text.insert(tk.END, message + '\n')
+    log_text.see(tk.END)
+
+
+
+
 # 设置DPI感知
 ctypes.windll.shcore.SetProcessDpiAwareness(2)
 # 创建主窗口
@@ -265,11 +279,11 @@ window = tk.Tk()
 
 
 window.title("Unity文件去混淆工具")
-window.geometry("620x300")
+window.geometry("5000x300")
 window.minsize(500, 300)  # 设置最小尺寸
 
 # 设置窗口居中
-window_width = 600
+window_width = 1080
 window_height = 400
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
@@ -308,12 +322,35 @@ scrollbar.grid(row=2, column=3, rowspan=6, sticky='ns')
 file_listbox = tk.Listbox(window, width=40, selectmode=tk.SINGLE, yscrollcommand=scrollbar.set)
 file_listbox.grid(row=2, column=2, rowspan=6, padx=10, pady=10, sticky='nsew')
 scrollbar.config(command=file_listbox.yview)
+
+
+# 创建日志显示控件
+log_label = tk.Label(window, text="日志：")
+log_label.grid(row=0, column=4, padx=10, pady=10, sticky='w')
+
+log_text = tk.Text(window, wrap=tk.WORD, state=tk.NORMAL, height=30, width=50)
+log_text.grid(row=1, column=4, rowspan=5, padx=10, pady=10, sticky='nsew')
+
+log_scrollbar = tk.Scrollbar(window, orient=tk.VERTICAL, command=log_text.yview)
+log_scrollbar.grid(row=1, column=5, rowspan=5, sticky='ns')
+log_text.config(yscrollcommand=log_scrollbar.set)
+
 # 读取文件内容
 open_mate_ini()
 
 # 配置行列权重
+window.grid_rowconfigure(0, weight=0)
+window.grid_rowconfigure(1, weight=1)
 window.grid_rowconfigure(2, weight=1)
+window.grid_rowconfigure(3, weight=1)
+window.grid_rowconfigure(4, weight=1)
+window.grid_rowconfigure(5, weight=1)
+window.grid_columnconfigure(0, weight=0)
+window.grid_columnconfigure(1, weight=1)
 window.grid_columnconfigure(2, weight=1)
+window.grid_columnconfigure(3, weight=1)
+window.grid_columnconfigure(4, weight=1)
+window.grid_columnconfigure(5, weight=1)
 
 # 创建并放置框架
 frame = tk.Frame(window)
